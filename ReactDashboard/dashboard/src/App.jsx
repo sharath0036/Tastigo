fc129ce21a1fff2ead05094e99d16497f82ba090
sharath0036 <ksharathbabu2@gmail.com>
2025-06-13 18:11:44 +0530

    frontend started

---
+++ ReactDashboard/dashboard/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingPage from './vendorDashboard/pages/LandingPage'
+
+const App = () => {
+  return (
+    <div>
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
