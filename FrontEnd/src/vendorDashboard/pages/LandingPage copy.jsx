e113c7792fa5c640e0d87cc2a07235108480cc1e
Srivasu215 <sri.vasu215@gmail.com>
2025-06-14 10:42:37 +0530

    short

---
+++ FrontEnd/src/vendorDashboard/pages/LandingPage copy.jsx
@@ -0,0 +1,68 @@
+import { useState } from 'react';
+import NavBar from '../components/NavBar';
+import SideBar from '../components/SideBar';
+import Login from '../components/forms/Login';
+import Register from '../components/forms/Register';
+import AddFirm from '../components/forms/AddFirm';
+import AddProducts from '../components/forms/AddProducts';
+
+const LandingPage = () => {
+
+  const [isLogin, setIsLogin] = useState(false);
+  const [isRegister, setIsRegister] = useState(false);
+  const [isAddFirm, setIsAddFirm] = useState(false);
+  const [isAddProducts, setIsAddProducts] = useState(false);
+
+  const loginHandler = () => {
+    setIsLogin(true);
+    setIsRegister(false);
+    setIsAddFirm(false);
+    setIsAddProducts(false);
+  }
+
+  const registerHandler = () => {
+    setIsRegister(true);
+    setIsLogin(false);
+    setIsAddFirm(false);
+    setIsAddProducts(false);
+  }
+
+  const addFirmHandler = () => {
+    setIsLogin(false);
+    setIsRegister(false);
+    setIsAddFirm(true);
+    setIsAddProducts(false);
+  }
+  const addProductsHandler = () => {
+    setIsLogin(false);
+    setIsRegister(false);
+    setIsAddFirm(false);
+    setIsAddProducts(true);
+  }
+  return (
+    <>
+      <div className="LandingPage">
+        <NavBar loginHandler={loginHandler} registerHandler={registerHandler} />
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-md-2">
+              <SideBar addFirmHandler={addFirmHandler} addProductsHandler={addProductsHandler} />
+
+            </div>
+            <div className="col-lg-9">
+              {/* Uncomment the following lines to enable login and register forms */}
+              {isLogin && <Login />}
+              {isRegister && <Register />}
+              {/* <Register/> */}
+              {isAddFirm && <AddFirm />}
+              {isAddProducts && <AddProducts />}
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LandingPage;
--- FrontEnd/src/vendorDashboard/pages/LandingPage.jsx
+++ FrontEnd/src/vendorDashboard/pages/LandingPage.jsx
@@ -7,61 +7,31 @@ import AddFirm from '../components/forms/AddFirm';
 import AddProducts from '../components/forms/AddProducts';
 
 const LandingPage = () => {
+  const [activeForm, setActiveForm] = useState(null);
 
-  const [isLogin, setIsLogin] = useState(false);
-  const [isRegister, setIsRegister] = useState(false);
-  const [isAddFirm, setIsAddFirm] = useState(false);
-  const [isAddProducts, setIsAddProducts] = useState(false);
-
-  const loginHandler = () => {
-    setIsLogin(true);
-    setIsRegister(false);
-    setIsAddFirm(false);
-    setIsAddProducts(false);
-  }
-
-  const registerHandler = () => {
-    setIsRegister(true);
-    setIsLogin(false);
-    setIsAddFirm(false);
-    setIsAddProducts(false);
-  }
-
-  const addFirmHandler = () => {
-    setIsLogin(false);
-    setIsRegister(false);
-    setIsAddFirm(true);
-    setIsAddProducts(false);
-  }
-  const addProductsHandler = () => {
-    setIsLogin(false);
-    setIsRegister(false);
-    setIsAddFirm(false);
-    setIsAddProducts(true);
-  }
   return (
-    <>
-      <div className="LandingPage">
-        <NavBar loginHandler={loginHandler} registerHandler={registerHandler} />
-        <div className="container-fluid">
-          <div className="row">
-            <div className="col-md-2">
-              <SideBar addFirmHandler={addFirmHandler} addProductsHandler={addProductsHandler} />
-
-            </div>
-            <div className="col-lg-9">
-              {/* Uncomment the following lines to enable login and register forms */}
-              {isLogin && <Login />}
-              {isRegister && <Register />}
-              {/* <Register/> */}
-              {isAddFirm && <AddFirm />}
-              {isAddProducts && <AddProducts />}
-
-            </div>
+    <div className="LandingPage">
+      <NavBar
+        loginHandler={() => setActiveForm("login")}
+        registerHandler={() => setActiveForm("register")}
+      />
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-2">
+            <SideBar
+              addFirmHandler={() => setActiveForm("addFirm")}
+              addProductsHandler={() => setActiveForm("addProducts")}
+            />
+          </div>
+          <div className="col-lg-9">
+            {activeForm === "login" && <Login />}
+            {activeForm === "register" && <Register />}
+            {activeForm === "addFirm" && <AddFirm />}
+            {activeForm === "addProducts" && <AddProducts />}
           </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
