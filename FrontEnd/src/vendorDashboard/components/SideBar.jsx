d3a01461a6a6ddef5363f51382f8b456e539a05a
Srivasu215 <sri.vasu215@gmail.com>
2025-06-14 10:34:31 +0530

    nav

--- FrontEnd/src/vendorDashboard/components/SideBar.jsx
+++ FrontEnd/src/vendorDashboard/components/SideBar.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+
+import { useState } from "react";
 
 const SideBar = ({ addFirmHandler, addProductsHandler }) => {
+    const [activeButton, setActiveButton] = useState("");
+
+    const handleClick = (buttonType, handler) => {
+        setActiveButton(buttonType);
+        handler();
+    };
+
     return (
         <div className="d-flex flex-column flex-shrink-0 p-3 bg-body-tertiary" style={{ width: '280px', height: '87vh' }}>
 
             <ul className="nav nav-pills flex-column mb-auto">
-                <li className="nav-item" onClick={addFirmHandler}>
-                    <a href="#" className="nav-link active" aria-current="page">
+                <li className="nav-item" onClick={() => handleClick("addfirm", addFirmHandler)}>
+                    <a href="#" className={`nav-link ${activeButton === "addfirm" ? "link-body-emphasis active" : "link-body-emphasis"}`} aria-current="page">
                         <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
                             <use href="#home" />
                         </svg>
@@ -14,7 +22,7 @@ const SideBar = ({ addFirmHandler, addProductsHandler }) => {
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link link-body-emphasis" onClick={addProductsHandler}>
+                    <a href="#" className={`nav-link ${activeButton === "addProducts" ? "link-body-emphasis active" : "link-body-emphasis"}`} onClick={() => handleClick("addProducts", addProductsHandler)} >
                         <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
                             <use href="#speedometer2" />
                         </svg>
@@ -22,7 +30,7 @@ const SideBar = ({ addFirmHandler, addProductsHandler }) => {
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link link-body-emphasis">
+                    <a href="#" className={`nav-link ${activeButton === "allProducts" ? "link-body-emphasis active" : "link-body-emphasis"}`}>
                         <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
                             <use href="#table" />
                         </svg>
@@ -30,7 +38,7 @@ const SideBar = ({ addFirmHandler, addProductsHandler }) => {
                     </a>
                 </li>
                 <li>
-                    <a href="#" className="nav-link link-body-emphasis">
+                    <a href="#" className={`nav-link ${activeButton === "userDetails" ? "link-body-emphasis active" : "link-body-emphasis"}`}>
                         <svg className="bi pe-none me-2" width="16" height="16" aria-hidden="true">
                             <use href="#grid" />
                         </svg>
--- FrontEnd/src/vendorDashboard/components/forms/AddFirm.jsx
+++ FrontEnd/src/vendorDashboard/components/forms/AddFirm.jsx
@@ -1,5 +1,3 @@
-import React from 'react'
-
 const AddFirm = () => {
   return (
     <div>
